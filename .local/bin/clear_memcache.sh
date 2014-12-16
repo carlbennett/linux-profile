4d2588ab606b9896264d6f6306e7ecd227f0b79a
Carl Bennett <Jailout2000@users.noreply.github.com>
2014-12-16 17:08:37 -0600

    Add clear memcache script, snowflakes script

---
+++ .local/bin/clear_memcache.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo 'flush_all' | nc localhost 11211
---
+++ .local/bin/snow.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+ 
+LINES=$(tput lines)
+COLUMNS=$(tput cols)
+ 
+declare -A snowflakes
+declare -A lastflakes
+
+trap 'printf "\n\e[1;31mKeyboard interrupt.\e[0;0m\n"; stty echo; exit 1' 2
+stty -echo
+clear
+ 
+function move_flake() {
+i="$1"
+ 
+if [ "${snowflakes[$i]}" = "" ] || [ "${snowflakes[$i]}" = "$LINES" ]; then
+	snowflakes[$i]=0
+else
+	if [ "${lastflakes[$i]}" != "" ]; then
+		printf "\033[%s;%sH \033[1;1H " ${lastflakes[$i]} $i
+	fi
+fi
+ 
+printf "\033[%s;%sH*\033[1;1H" ${snowflakes[$i]} $i
+ 
+lastflakes[$i]=${snowflakes[$i]}
+snowflakes[$i]=$((${snowflakes[$i]}+1))
+}
+ 
+while :
+do
+	i=$(($RANDOM % $COLUMNS))
+	 
+	move_flake $i
+	 
+	for x in "${!lastflakes[@]}"
+	do
+		move_flake "$x"
+	done
+	 
+	sleep 0.1
+done
+
+stty echo
+
