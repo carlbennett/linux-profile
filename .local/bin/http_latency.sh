9798a56a207da8706289e7139068c4c6ad0b581e
Carl Bennett <Jailout2000@users.noreply.github.com>
2015-02-28 22:54:46 +0000

    Add usage string to http latency script.

--- .local/bin/http_latency.sh
+++ .local/bin/http_latency.sh
@@ -2,5 +2,6 @@
 CURL="/usr/bin/curl"
 AWK="/usr/bin/awk"
 URL="$1"
+[ "$URL" == "" ] && printf "Usage: $0 <url>\n" && exit 0
 result=`$CURL -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total} $URL`
 echo $result | $AWK -F: '{ print $1" "$2" "$3}'
