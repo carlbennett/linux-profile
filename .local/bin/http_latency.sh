231860c1f07df805777cda622c4d70b8d7ce3578
Carl Bennett <carl@carlbennett.me>
2025-09-17 02:12:04 -0500

    Remove unused local scripts

--- .local/bin/http_latency.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-CURL="/usr/bin/curl"
-AWK="/usr/bin/awk"
-URL="$1"
-[ "$URL" == "" ] && printf "Usage: $0 <url>\n" && exit 0
-result=`$CURL -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total} $URL`
-echo $result | $AWK -F: '{ print $1" "$2" "$3}'
